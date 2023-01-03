5b2ae0dd554ce80cfb560defde717ff9eaf8b3e1
Evandro <erissatto@gmail.com>
2023-01-03 14:27:46 -0300

    new text in home page

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App({ signOut, user }) {
   return (
     <div>
       <img src={logo} className="App-logo" alt="logo" />
-      <h1>Hello! now with auth{user.username}</h1>
+      <h1>Hello! Agora com autenticação de usuário!!! Que tal!? {user.username}</h1>
       <button onClick={signOut}>Sign out</button>
       </div>
   );
